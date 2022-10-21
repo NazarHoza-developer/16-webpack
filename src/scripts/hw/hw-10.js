43123b98e8e12b78c4df59228be4f62fa6e3b963
nazarhoza <nazarhoza@gmail.com>
2022-10-21 16:54:23 +0300

    new repo

--- src/scripts/hw/hw-10.js
+++ src/scripts/hw/hw-10.js
@@ -16,27 +16,3 @@ export function addDiv() {
         return arrColors[Math.floor(Math.random(0, 7) * 10)];
     }
   };
-// let wrapperDiv = document.createElement("div");
-// wrapperDiv.className = "wrapper";
-// document.getElementsByTagName("body")[0].appendChild(wrapperDiv)
-
-// for (let i = 0; i < 25; i++) {
-//     let divBoxes = document.createElement("div");
-//     divBoxes.className = "box";
-//     wrapperDiv.appendChild(divBoxes);
-// }
-
-// function generateBlocks() {
-//     const oneRGB = () => Math.floor(Math.random() * 256);
-//     const fullRGB = () => "rgb(" + oneRGB() + "," + oneRGB() + "," + oneRGB() + ")";
-//     const boxes = Array.from(document.getElementsByClassName('box'));
-//     boxes.map((el) => {
-//         el.style.backgroundColor = fullRGB();
-//     });
-// }
-
-// function generateBlocksInterval() {
-//     setInterval(generateBlocks, 1000)
-// }
-
-// generateBlocksInterval();
